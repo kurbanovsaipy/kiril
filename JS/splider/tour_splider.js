6a65ff5c18273d9fb968eb0674df22ccb9ecf892
kurbanovsaipy <131548068+kurbanovsaipy@users.noreply.github.com>
2024-01-17 12:40:32 +0300

    kiril

---
+++ JS/splider/tour_splider.js
@@ -0,0 +1,10 @@
+var tour = new Splide( '.tour_splider', {
+    focus  : 0,
+    omitEnd: true,
+    autoWidth: true,
+    autoHeight: true,
+    gap: '50px',
+    padding: '0px',
+  } );
+  
+  tour.mount();
